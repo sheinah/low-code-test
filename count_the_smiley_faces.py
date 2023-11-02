dcdaf5ee0f31790ac002f861c45828f181ca1182
Sheniah.CH <104757175+sheinah@users.noreply.github.com>
2023-11-02 21:05:11 +0700

    error-handling & checking

--- count_the_smiley_faces.py
+++ count_the_smiley_faces.py
@@ -3,6 +3,10 @@ import re
 def countSmileys(arr):
     if not arr:
         return 0
+    
+    """Count the number of smilies in a list of strings"""
+    assert type(arr) == list, "arr must be list"
+    assert all([type(s) == str for s in arr]), "all elements of arr must be strings"
 
     # Define a regular expression pattern to match valid smiley faces
     pattern = r"[:;][-~]?[)D]"
@@ -18,3 +22,29 @@ print(countSmileys([]))
 print(countSmileys([':)', ';(', ';}', ':-D']))
 print(countSmileys([';D', ':-(', ':-)', ';~)']))
 print(countSmileys([';]', ':[', ';*', ':$', ';-D']))
+
+
+# Example test cases, including the case of an empty array
+assert countSmileys([]) == 0
+assert countSmileys([":)", ";(", ";}", ":-D"]) == 2
+assert countSmileys([";D", ":-(", ":-)", ";~)"]) == 3
+assert countSmileys([";]", ":[", ";*", ":$", ";-D"]) == 1
+assert countSmileys([";]", ":[", ";*", ":$", ";-D"]) == 1
+assert countSmileys([";]", ":[", ";*", ":$", ";-D", "wefwe", "df", ""]) == 1
+assert countSmileys([";", "df", "", ")))))))))))))))))", ";;", "(:"]) == 0
+
+# Check the assertions
+try:
+    countSmileys(":)")
+except AssertionError as _:
+    pass
+else:
+    raise AssertionError("Bad input accepted")
+
+try:
+    countSmileys([":)", 220])
+except AssertionError as _:
+    pass
+else:
+    raise AssertionError("Bad input accepted")
+
